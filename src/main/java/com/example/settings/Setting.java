29936c53902487fa36a35933993576bf951eb0d7
mkroczek <1marcin100@gmail.com>
2022-06-24 23:17:32 +0200

    Implement tests for AlgorithmSetting and TextField setting input

--- src/main/java/com/example/settings/Setting.java
+++ src/main/java/com/example/settings/Setting.java
@@ -3,7 +3,7 @@ package com.example.settings;
 public interface Setting<T> {
     String getName();
     Boolean isProperValue(T value);
-    void setValue(T value);
+    void setValue(T value) throws IllegalArgumentException;
     Class<T> getContainedClass();
     T getValue();
 }
