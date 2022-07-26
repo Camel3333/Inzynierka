d47419d6c8591cd3de125e31d555a35f1031c382
syklzz <80258654+syklzz@users.noreply.github.com>
2022-07-26 23:34:42 +0200

    clean up code

--- src/main/java/com/example/simulation/SimpleSimulation.java
+++ src/main/java/com/example/simulation/SimpleSimulation.java
@@ -1,7 +1,6 @@
 package com.example.simulation;
 
 import com.example.algorithm.Algorithm;
-import com.example.algorithm.operations.Operation;
 import com.example.algorithm.report.StepReport;
 import com.example.animation.AnimationEngine;
 import com.example.animation.AnimationEngineFactory;
@@ -10,15 +9,10 @@ import com.example.controller.GraphController;
 import com.example.model.MyGraph;
 import javafx.beans.property.BooleanProperty;
 import javafx.beans.property.SimpleBooleanProperty;
-import javafx.concurrent.Service;
-import javafx.concurrent.Task;
-import javafx.concurrent.WorkerStateEvent;
-import javafx.event.EventHandler;
 import lombok.Setter;
 
-import java.util.List;
 
-public class SimpleSimulation extends Service<Boolean> implements Simulation{
+public class SimpleSimulation implements Simulation{
 
     /*
     STEP -> zwraca klasę reprezentująca zmiany
@@ -36,12 +30,6 @@ public class SimpleSimulation extends Service<Boolean> implements Simulation{
 
     public SimpleSimulation(GraphController graphController) {
         this.graphController = graphController;
-        setOnSucceeded(new EventHandler<WorkerStateEvent>() {
-            @Override
-            public void handle(WorkerStateEvent event) {
-                System.out.println("Algorithm succeeded: "+event.getSource().getValue());
-            }
-        });
     }
 
     public void setEnvironment(Algorithm algorithm, AlgorithmSettings settings){
@@ -55,38 +43,8 @@ public class SimpleSimulation extends Service<Boolean> implements Simulation{
         allowAnimations.setValue(allow);
     }
 
-    @Override
-    public void startSimulation() {
-        algorithm.loadEnvironment((MyGraph<Integer, Integer>) graphController.getGraph(), settings);
-        while (!algorithm.isFinished()){
-            StepReport report = algorithm.step();
-            // simulate animation
-            if (allowAnimations.get()){
-                animationEngine.animate(report);
-            }
-            // collect statistics
-        }
-        graphController.update();
-    }
-
-    @Override
-    protected Task<Boolean> createTask() {
-        return new Task<Boolean>() {
-            @Override
-            protected Boolean call() throws Exception {
-                startSimulation();
-                return true;
-            }
-        };
-    }
-
-
     public void loadEnvironment() {
         algorithm.loadEnvironment((MyGraph<Integer, Integer>) graphController.getGraph(), settings);
-    }
-
-    public boolean isFinished() {
-        return algorithm.isFinished();
     }
 
     public BooleanProperty getIsFinishedProperty() {
