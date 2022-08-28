fba53f02a27dd5ea8d64ec6ee57c80746fdd0510
Kamil <kamilzubko@student.agh.edu.pl>
2022-08-28 18:02:30 +0200

    Added color legend

---
+++ src/main/java/com/example/controller/LegendController.java
@@ -0,0 +1,14 @@
+package com.example.controller;
+
+import javafx.fxml.FXML;
+import net.rgielen.fxweaver.core.FxmlView;
+import org.springframework.stereotype.Component;
+
+@Component
+@FxmlView("/view/statisticsView.fxml")
+public class LegendController {
+
+    @FXML
+    public void initialize() {
+    }
+}
