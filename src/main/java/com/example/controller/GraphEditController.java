0258c3337bd692509d18c24fd2af3d0640c7a1a7
syklzz <80258654+syklzz@users.noreply.github.com>
2022-09-26 20:41:55 +0200

    bg 126 move simulation buttons to toolbar (#38)
    * move simulation buttons to toolbar
    
    * refactor - extract variables
    
    * fixes

--- src/main/java/com/example/controller/GraphEditController.java
+++ src/main/java/com/example/controller/GraphEditController.java
@@ -29,11 +29,22 @@ public class GraphEditController {
     @FXML
     private Button simulateButton;
     @FXML
+    private Button startButton;
+    @FXML
+    private Button nextStepButton;
+    @FXML
+    private Button liveButton;
+    @FXML
+    private Button instantFinishButton;
+    @FXML
+    private Button pauseButton;
+    @FXML
     private Button drawButton;
 
 
     private DrawMenuController drawMenuController;
     private SimulationMenuController simulationMenuController;
+    private SimulationController simulationController;
 
     private void initializeDrawingButtons() {
         vertexButton.setOnAction(e -> drawMenuController.selectMode(DrawMode.VERTEX));
@@ -45,10 +56,15 @@ public class GraphEditController {
     }
 
     private void initializeSimulationButtons() {
+        startButton.setOnAction(e -> simulationMenuController.startItem.fire());
+        nextStepButton.setOnAction(e -> simulationMenuController.nextStepItem.fire());
+        liveButton.setOnAction(e -> simulationMenuController.liveItem.fire());
+        instantFinishButton.setOnAction(e -> simulationMenuController.instantFinishItem.fire());
+        pauseButton.setOnAction(e -> simulationMenuController.pauseItem.fire());
         simulateButton.setOnAction(e -> simulationMenuController.changeApplicationState(ApplicationState.SIMULATING));
         drawButton.setOnAction(e -> simulationMenuController.changeApplicationState(ApplicationState.DRAWING));
         buttons.put(ApplicationState.SIMULATING,
-                new ArrayList<>(List.of(drawButton)));
+                new ArrayList<>(List.of(startButton, nextStepButton, liveButton, instantFinishButton, pauseButton, drawButton)));
     }
 
     @FXML
@@ -65,6 +81,11 @@ public class GraphEditController {
         simulationMenuController = controller;
     }
 
+    public void setSimulationController(SimulationController simulationController) {
+        this.simulationController = simulationController;
+        bindButtons();
+    }
+
     public void setEnabled(boolean enabled, ApplicationState applicationState) {
         buttons.get(applicationState).forEach(button -> {
             button.setManaged(enabled);
@@ -75,5 +96,13 @@ public class GraphEditController {
     public void setChaneStateToSimulationEnabled(boolean enabled) {
         simulateButton.setManaged(enabled);
         simulateButton.setVisible(enabled);
+    }
+
+    public void bindButtons() {
+        startButton.disableProperty().bind(simulationController.getStartDisabledProperty());
+        nextStepButton.disableProperty().bind(simulationController.getNextStepDisabledProperty());
+        liveButton.disableProperty().bind(simulationController.getLiveDisabledProperty());
+        instantFinishButton.disableProperty().bind(simulationController.getInstantFinishDisabledProperty());
+        pauseButton.disableProperty().bind(simulationController.getPauseDisabledProperty());
     }
 }
