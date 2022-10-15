499b8cd4ffc628a9b4ec6155f51e521672854b7d
mkroczek <73397853+mkroczek@users.noreply.github.com>
2022-10-15 19:00:32 +0200

    Bugfix/bg 155 start button fix (#56)
    * Stable version
    
    * Small cleanup

---
+++ src/main/java/com/example/controller/settings/KingSettingsController.java
@@ -0,0 +1,57 @@
+package com.example.controller.settings;
+
+import com.example.model.MyGraph;
+import com.example.settings.*;
+import javafx.beans.property.BooleanProperty;
+import javafx.fxml.FXML;
+import javafx.scene.Node;
+import javafx.scene.control.Label;
+import lombok.Getter;
+import net.rgielen.fxweaver.core.FxmlView;
+import org.springframework.stereotype.Component;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Component
+@FxmlView("/view/settings/kingSettingsView.fxml")
+public class KingSettingsController implements AlgorithmSettingsController {
+    @Getter
+    @FXML
+    public Node parent;
+    @FXML
+    public Label phaseLabel;
+    @FXML
+    private IntegerSettingTextField phase;
+    private SettingNodesGroup settingNodesGroup;
+    @Getter
+    private AlgorithmSettings algorithmSettings = new AlgorithmSettings();
+
+    @FXML
+    public void initialize() {
+        setDefaultSettings();
+        settingNodesGroup = new SettingNodesGroupImpl(Collections.singletonList(phase));
+    }
+
+    @Override
+    public BooleanProperty getAreSettingsValidProperty() {
+        return settingNodesGroup.getAreAllValidProperty();
+    }
+
+    @Override
+    public List<Node> getAllNodes() {
+        return settingNodesGroup.getAllNodes().stream().map(settingNode -> (Node) settingNode).collect(Collectors.toList());
+    }
+
+    @Override
+    public void adjustSettingsConditions(MyGraph<Integer, Integer> graph) {
+        // intentionally do nothing - settings do not depend on graph
+    }
+
+    private void setDefaultSettings() {
+        AlgorithmSetting<Integer> phaseSetting = new AlgorithmSetting<>("phase", 1, Integer.class, (value) -> value > 0);
+        algorithmSettings.getSettings().put("phase", phaseSetting);
+        phase.setContainedSetting(phaseSetting);
+    }
+}
